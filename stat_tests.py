61fbf05e305b093ba1c7a960fdc71660a932c5a2
DanielYu <thgreatexplorer@gmail.com>
2023-11-17 13:47:36 -0500

    Added in new csv files for shorter time period, past 6 months, 3Hr intrady. Changed Visualizations to be more descriptive

--- stat_tests.py
+++ stat_tests.py
@@ -1,1 +1,30 @@
 # TODO Logic of checking the co-integration of two pairs
+
+import numpy as np
+import pandas as pd
+from pandas import Series
+
+import statsmodels
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import coint, adfuller
+from itertools import combinations
+
+import matplotlib.pyplot as plt
+
+class BondPairAnalysis:
+    """
+    Class to handle all the data processing. Main responsibility is to return the viable pairs of stocks which are cointegrated 
+    (and whose linear combination also passes stationarity). Auxiliary reponsibilities include analyzing and visualizing the 
+    time series.
+    """
+
+    # TODO fields
+
+    def __init__(self) -> None:
+        pass
+    
+    def process_csv(csv_file_path :str) -> Series:
+        raw_csv = pd.read_csv(csv_file_path)
+        
+
+
